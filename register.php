f3e7aa84115776bbf9e2aa35150c66d7e6058996
knctlln <castillano.kaneerryl@auf.edu.ph>
2022-09-03 15:31:40 +0800

    first commit

---
+++ register.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title> PDC 10 Lab Activity </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+    <form>
+    <h1 style= margin: 35px> Registration Form </h1>
+      <br>
+  <div class="container">
+  <div class="form-group row">
+    <label for="inputName" class="col-sm-2 col-form-label">Complete Name</label>
+    <div class="col-sm-10">
+      <input type="name" class="form-control" id="inputName" placeholder="Name">
+    </div>
+  </div>
+  <br>
+  <div class="form-group row">
+    <label for="inputEmail" class="col-sm-2 col-form-label">Email Address</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+    </div>
+  </div>
+  <br>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <br>
+  <div class="form-group row">
+    <label for="inputPassword2" class="col-sm-2 col-form-label">Confirm Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Confirm Password">
+    </div>
+  </div>
+  <br>
+  <div class="form-group row">
+    <label for="inputPicture" class="col-sm-2 col-form-label">Picture</label>
+    <div class="col-sm-10">
+    <button type="submit" class="btn btn-primary"> Attach Picture </button>
+    </div>
+  </div>
+  <br>
+  <div class="text-center">
+    <div class="col-sm-auto">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </div>
+</div>
+</form>
